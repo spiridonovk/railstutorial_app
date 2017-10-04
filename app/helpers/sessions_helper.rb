3f7cdaab114370c83f5c6e61a469d8f6af536149
zero75 <master4842@yandex.ru>
2017-10-04 22:51:28 +0300

    remember me

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,10 +5,18 @@ module SessionsHelper
     session[:user_id] = user.id
      @current_user ||= User.find_by(id: session[:user_id])
   end
+   # Запоминает пользователя в постоянную сессию.
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+
   #Returns true if the user is logged in, false otherwise.
   def logged_in?
     !current_user.nil?
   end
+
    # Returns the current logged-in user (if any).
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
@@ -18,4 +26,18 @@ module SessionsHelper
     session.delete(:user_id)
     @current_user = nil
   end
+  # Забывает постоянную сессии.
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+
+  # Осуществляет выход текущего пользователя.
+  def log_out
+    forget(current_user)
+    session.delete(:user_id)
+    @current_user = nil
+  end
 end
+
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,12 @@
 class User < ApplicationRecord
+   attr_accessor :remember_token
    before_save { self.email = email.downcase }
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },  uniqueness: true,
                     uniqueness: { case_sensitive: false }
+  has_secure_password
   validates :password, presence: true, length: {minimum: 6}
 # Returns the hash digest of the given string.
   def User.digest(string)
@@ -12,5 +14,22 @@ class User < ApplicationRecord
                                                   BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
   end
- has_secure_password
+
+ # Возвращает случайный токен.
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  # Запоминает пользователя в базе данных для использования в постоянной сессии.
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+  # Возвращает true, если предоставленный токен совпадает с дайджестом.
+  def authenticated?(remember_token)
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+   def forget
+    update_attribute(:remember_digest, nil)
+  end
 end
